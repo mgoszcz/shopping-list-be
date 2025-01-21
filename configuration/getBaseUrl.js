c365d4bd08531eba7d5fe00de59818ac91e26287
Marcin Goszczynski <61630391+mgoszcz@users.noreply.github.com>
2025-01-21 14:25:42 +0100

    Setup GitHub actions (#11)

--- configuration/getBaseUrl.js
+++ configuration/getBaseUrl.js
@@ -4,7 +4,7 @@ const getBaseUrl = () => {
   const environment = process.env.ENVIRONMENT || "prod";
   if (environment === "local") {
     return "http://localhost:3000";
-  } else if (environment === "develop") {
+  } else if (environment === "development") {
     return "https://shopping-list-be-development.up.railway.app";
   } else {
     throw new Error(`Unknown environment: ${environment}`);
