4f64105a214ec92883e7d6c9d573acc967145604
Marcin Goszczynski <marcin.goszczynski@imgarena.com>
2025-02-02 15:04:36 +0100

    update response data for shopping cart and shopping articles post and put

--- configuration/getBaseUrl.js
+++ configuration/getBaseUrl.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const getBaseUrl = () => {
   const environment = process.env.ENVIRONMENT || "prod";
   if (environment === "local") {
-    return "http://localhost:3000";
+    return "http://localhost:3001";
   } else if (environment === "development") {
     return "https://shopping-list-be-development.up.railway.app";
   } else {
