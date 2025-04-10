7825b062c99450ab4614497e5587f40a67bb5cc1
mgoszczynski <marcin.goszczynski88@gmail.com>
2025-04-10 22:50:40 +0200

    Add test case for updating shopping cart timestamp

--- tests/lastModified/lastModifiedShopCategories.test.js
+++ tests/lastModified/lastModifiedShopCategories.test.js
@@ -5,14 +5,28 @@ const {
   shopCategoriesEndpoint,
   shoppingCartEndpoint,
   shoppingArticlesEndpoint,
+  lastModifiedShoppingCartEndpoint,
 } = require("../consts/urls");
 const { expect, describe } = require("@jest/globals");
 
+const getTimestampBefore = async (endpoint) => {
+  let timestampBefore;
+  try {
+    timestampBefore = (await axios.get(endpoint)).data.lastModified;
+  } catch (error) {
+    timestampBefore = 0;
+  }
+  return timestampBefore;
+};
+
 describe("Last modified endpoint for ShopCategories", () => {
-  test("PUT updating shop category should update last modified date of ShopCategories table", async () => {
-    const timestampBefore = (
-      await axios.get(lastModifiedShopCategoriesEndpoint)
-    ).data.lastModified;
+  test("PUT updating shop category should update last modified date of ShopCategories and ShoppingCart table", async () => {
+    const categoriesTimestampBefore = await getTimestampBefore(
+      lastModifiedShopCategoriesEndpoint
+    );
+    const shoppingCartTimestampBefore = await getTimestampBefore(
+      lastModifiedShoppingCartEndpoint
+    );
     await axios.put(shopCategoriesEndpoint(1), [
       {
         category: {
@@ -27,17 +41,40 @@ describe("Last modified endpoint for ShopCategories", () => {
         category_order: 2,
       },
     ]);
-    const response = await axios.get(lastModifiedShopCategoriesEndpoint);
-    expect(response.data.lastModified).not.toBe(timestampBefore);
+    const categoriesResponse = await axios.get(
+      lastModifiedShopCategoriesEndpoint
+    );
+    expect(categoriesResponse.data.lastModified).not.toBe(
+      categoriesTimestampBefore
+    );
+    const shoppingCartResponse = await axios.get(
+      lastModifiedShoppingCartEndpoint
+    );
+    expect(shoppingCartResponse.data.lastModified).not.toBe(
+      shoppingCartTimestampBefore
+    );
   });
 
-  test("PUT removing categories for shop should update last modified date of ShopCategories table", async () => {
-    const timestampBefore = (
-      await axios.get(lastModifiedShopCategoriesEndpoint)
-    ).data.lastModified;
+  test("PUT removing categories for shop should update last modified date of ShopCategories and ShoppingCart table", async () => {
+    const categoriesTimestampBefore = await getTimestampBefore(
+      lastModifiedShopCategoriesEndpoint
+    );
+    const shoppingCartTimestampBefore = await getTimestampBefore(
+      lastModifiedShoppingCartEndpoint
+    );
     await axios.put(shopCategoriesEndpoint(1), []);
-    const response = await axios.get(lastModifiedShopCategoriesEndpoint);
-    expect(response.data.lastModified).not.toBe(timestampBefore);
+    const categoriesResponse = await axios.get(
+      lastModifiedShopCategoriesEndpoint
+    );
+    expect(categoriesResponse.data.lastModified).not.toBe(
+      categoriesTimestampBefore
+    );
+    const shoppingCartResponse = await axios.get(
+      lastModifiedShoppingCartEndpoint
+    );
+    expect(shoppingCartResponse.data.lastModified).not.toBe(
+      shoppingCartTimestampBefore
+    );
   });
 
   test("removing shop that is in shop categories should update last modified date of ShopCategories table", async () => {
